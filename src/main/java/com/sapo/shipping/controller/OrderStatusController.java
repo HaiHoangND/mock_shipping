5e27443486a0acc7223cd8ba027608791fd2fb26
unknown <longtp@gmail.com>
2023-08-29 16:53:17 +0700

    final day 1

--- src/main/java/com/sapo/shipping/controller/OrderStatusController.java
+++ src/main/java/com/sapo/shipping/controller/OrderStatusController.java
@@ -1,8 +1,9 @@
 package com.sapo.shipping.controller;
 
+import com.sapo.shipping.dto.OrderStatusDto;
+import com.sapo.shipping.response.GeneralResponse;
 import com.sapo.shipping.service.impl.OrderStatusService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/api/orderStatus")
@@ -11,5 +12,33 @@ public class OrderStatusController {
 
     public OrderStatusController(OrderStatusService orderStatusService) {
         this.orderStatusService = orderStatusService;
+    }
+
+    @GetMapping("{id}")
+    GeneralResponse<?> getOrderStatusById(@PathVariable int id) {
+        return GeneralResponse.ok("success",
+                "Successfully fetched",
+                orderStatusService.getById(id));
+    }
+
+    @PostMapping
+    GeneralResponse<?> createOrderStatus(@RequestBody OrderStatusDto orderStatusDto) {
+        return GeneralResponse.ok("success",
+                "Successfully created",
+                orderStatusService.create(orderStatusDto));
+    }
+
+    @PutMapping("{id}")
+    GeneralResponse<?> updateOrderStatus(@PathVariable int id, @RequestBody OrderStatusDto orderStatusDto) {
+        return GeneralResponse.ok("success",
+                "Successfully created",
+                orderStatusService.update(id, orderStatusDto));
+    }
+
+    @DeleteMapping("{id}")
+    GeneralResponse<?> deleteOrderStatusById(@PathVariable int id){
+        return GeneralResponse.ok("success",
+                "Successfully deleted",
+                orderStatusService.delete(id));
     }
 }
--- src/main/java/com/sapo/shipping/service/impl/OrderStatusService.java
+++ src/main/java/com/sapo/shipping/service/impl/OrderStatusService.java
@@ -2,36 +2,68 @@ package com.sapo.shipping.service.impl;
 
 import com.sapo.shipping.dto.OrderStatusDto;
 import com.sapo.shipping.entity.OrderStatus;
+import com.sapo.shipping.exception.BusinessException;
+import com.sapo.shipping.mapper.OrderStatusMapper;
 import com.sapo.shipping.repository.OrderStatusRepository;
 import com.sapo.shipping.service.IOrderStatusService;
+import jakarta.transaction.Transactional;
+import jakarta.validation.Validator;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class OrderStatusService implements IOrderStatusService {
 
     private final OrderStatusRepository orderStatusRepository;
+    private final OrderStatusMapper mapper;
+    private final Validator validator;
 
-    public OrderStatusService(OrderStatusRepository orderStatusRepository) {
+    public OrderStatusService(OrderStatusRepository orderStatusRepository, OrderStatusMapper mapper, Validator validator) {
         this.orderStatusRepository = orderStatusRepository;
+        this.mapper = mapper;
+        this.validator = validator;
     }
 
     @Override
     public OrderStatus getById(int id) {
-        return null;
+        return orderStatusRepository.findById(id)
+                .orElseThrow(() -> new BusinessException("404", "error", "Order not found"));
     }
 
     @Override
     public OrderStatus create(OrderStatusDto orderStatusDto) {
-        return null;
+        List<String> errors = new ArrayList<>();
+        validator.validate(orderStatusDto)
+                .forEach(e -> errors.add(e.getMessage()));
+        if (!errors.isEmpty()) {
+            throw new BusinessException("400", "error", errors.get(0));
+        }
+        OrderStatus orderStatus = mapper.createEntity(orderStatusDto);
+        return orderStatusRepository.save(orderStatus);
     }
 
     @Override
+    @Transactional(rollbackOn = Exception.class)
     public OrderStatus update(int id, OrderStatusDto orderStatusDto) {
-        return null;
+        List<String> errors = new ArrayList<>();
+        validator.validate(orderStatusDto)
+                .forEach(e -> errors.add(e.getMessage()));
+        if (!errors.isEmpty()) {
+            throw new BusinessException("400", "error", errors.get(0));
+        }
+        OrderStatus orderStatus = orderStatusRepository.findById(id)
+                .orElseThrow(() -> new BusinessException("404", "error", "Order not found"));
+        mapper.updateEntity(orderStatus, orderStatusDto);
+        return orderStatusRepository.save(orderStatus);
     }
 
     @Override
     public Boolean delete(int id) {
-        return null;
+        orderStatusRepository.findById(id)
+                .orElseThrow(()-> new BusinessException("404", "error", "Order not found"));
+        orderStatusRepository.deleteById(id);
+        return true;
     }
 }
